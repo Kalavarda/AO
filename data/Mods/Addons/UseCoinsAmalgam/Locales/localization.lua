a06957a1a755a6ce317f7d8ea77f16e141a1bea7
kalavarda <test@test.com>
2021-02-28 10:57:29 +0300

    1

---
+++ data/Mods/Addons/UseCoinsAmalgam/Locales/localization.lua
@@ -0,0 +1,38 @@
+-- remove items by adding "--" for example: --["Пыльца фей"] = true,
+-- удалить элементы, добавив "--" например: --["Пыльца фей"] = true,
+--------------------------------------------------------------------------------
+-- Все языки | All languages
+--------------------------------------------------------------------------------
+-- items used for valuable tab
+-- предметы, используемые для ценных вкладок
+Global("itemNames", {
+	--rus
+	["Жетон Исследователей Полигона"] = true,
+	["Коллекционная монета Олигархов"] = true,
+	["Осколок Ваятеля"] = true,
+	["Пыльца фей"] = true,
+	["Кошель с Экю"] = true,
+	--eng_eu
+	["Tycoon Collector's Coin"] = true,
+	["Sculptor's Shard"] = true,
+	["Training Camp Explorer Token"] = true,
+	["Fairy Dust"] = true,
+	["Ecu"] = true,
+	["Purse of Ecu"] = true,
+	--tur
+	--ger
+	--fr
+})
+-- items that are combined
+-- предметы, которые комбинируются
+Global("amalgamNames", {
+	--rus
+	["Капли амальгамы"] = true,
+	["Крупицы реальгара"] = true,
+	--eng_eu
+	["Drops of Amalgam"] = true,
+	["Grains of Realgar"] = true,
+	--tur
+	--ger
+	--fr
+})
