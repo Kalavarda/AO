2a44f39e2de42f9c20c5b878545ad13b20ef1ef9
kalavarda <test@test.com>
2021-04-18 12:29:36 +0300

    1

---
+++ data/Mods/Addons/BVFarm/BVFarm.lua
@@ -0,0 +1,184 @@
+-- -----------------------------------------------------
+-- ---------- ! Настройка аддона здесь ! ---------------
+local Merc = {
+
+-- Чтобы изменить список вызываемых наёмников, надо закомментировать
+-- строку с именем ненужного наёмника (поставить два минуса в начале
+-- строки) или раскомментировать строку с нужным (убрать эти минусы).
+-- Например, сейчас закомментирована строка с Бурей, поэтому она не
+-- вызывается.
+
+	"Громила Лоб Буйных",
+	"Лекарь Матрёна Коновалова",
+--	"Стрелок Кузьма Орлов",
+--	"Рубака Буря Степных",
+	"Ведунья Лиза ди Вевр",
+--	"Колдун Михаил Грамотин",
+   }
+
+-- Автозапуск битвы при разговоре с НПС (аддон выбирает "Начнём!" в диалоге).
+-- Чтобы выключить этот автозапуск, замените true на false.
+
+local AutoStartBattle = true
+
+-- --------- ! конец области с настройкам ! ------------
+-- -----------------------------------------------------
+
+
+local MapName = "Безвременье"
+local MercOffc = { "Александра Сольдина", "Властемир Наймитов" }
+local HireMercLine = "Нанять наёмника"
+local ReviveMercLine = "Воскресить наёмника"
+
+local BattleNPC = {
+	"Ерофей Хранилов",
+	"Нимра Озёрнова",
+	"Тереза ди Ардер",
+	"Дух Света",
+	"Степан Триглавин",
+	"Матёрый мечник",
+	"Искусная волшебница",
+	"Угрюмый гридень"
+}
+local StartBattleLine = "Начнём!"
+
+-- Функция: Событие начала разговора с NPC
+function OnTalkStarted()
+
+   local currentMap = cartographer.GetCurrentMapInfo()
+   if userMods.FromWString(currentMap.name) == MapName then
+
+	local info = avatar.GetInteractorInfo()
+
+	if info.hasInteraction then
+
+		if userMods.FromWString(object.GetName(avatar.GetInterlocutor())) == MercOffc[1] or userMods.FromWString(object.GetName(avatar.GetInterlocutor())) == MercOffc[2] then
+			if TalkOptionPresent(ReviveMercLine) then
+				SelectTalkOption(ReviveMercLine)
+				for i=1, GetTableSize(Merc) do
+					SelectTalkOption(Merc[i])
+				end
+			else
+				SelectTalkOption(HireMercLine)
+				for i=1, GetTableSize(Merc) do
+					SelectTalkOption(Merc[i])
+				end
+			end
+		end
+
+		for i=1, GetTableSize(BattleNPC) do
+			if AutoStartBattle and userMods.FromWString(object.GetName(avatar.GetInterlocutor())) == BattleNPC[i] then
+				SelectTalkOption(StartBattleLine)
+			end
+		end
+
+	end
+   end
+end
+
+
+-- Функция: Выбрать эту опцию в разговоре
+
+function SelectTalkOption(talkOption)
+
+	local SpisReplik = avatar.GetInteractorNextCues()
+	for i=0, GetTableSize(SpisReplik) do
+		if SpisReplik[i] then
+			if userMods.FromWString(SpisReplik[i].name) == talkOption then
+				avatar.SelectInteractorCue(i)
+			end
+		end
+	end
+end
+
+-- Функция: Если ли эта опция в разговоре
+
+function TalkOptionPresent(talkOption)
+
+	local SpisReplik = avatar.GetInteractorNextCues()
+	for i=0, GetTableSize(SpisReplik) do
+		if SpisReplik[i] then
+			if userMods.FromWString(SpisReplik[i].name) == talkOption then
+				return true
+			end
+		end
+	end
+end
+
+
+
+-- ------------------------
+
+local GetContextActionShortInfo = avatar.GetContextActionShortInfo or avatar.GetContextActionInfo
+local IsNavigateToPoint = avatar.IsNavigateToPoint or function() return false end
+local ExtractWStringFromValuedText = function(str) return common.IsWString(str) and str or common.ExtractWStringFromValuedText(str) end
+
+local activeNPC
+local knownNPCs = {}
+local retries
+local maxRetries = 20
+
+function OnActionFailed( params )
+	if activeNPC and params.sysId == "ENUM_ActionFailCause_TooFar" and not params.isInNotPredicate then
+		if not retries then retries = maxRetries end
+		if retries > 0 then
+			retries = retries - 1
+			-- Try again
+			knownNPCs = {}
+			OnContextActionsChanged()
+		else
+			-- It's enough
+			retries = nil
+		end
+	end
+end
+
+function OnContextActionsChanged()
+	local currentMap = cartographer.GetCurrentMapInfo()
+	local current = {}
+	local flag = false
+	local actions = avatar.GetContextActions()
+	for _,action in pairs(actions) do
+		local info = GetContextActionShortInfo(action)
+		if info and info.objectId and info.enabled and info.sysType == "ENUM_CONTEXT_ACTION_TYPE_NPC_TALK" then
+			local name = userMods.FromWString(object.GetName(info.objectId))
+			if not knownNPCs[name] then
+				flag = true
+			end
+			current[name] = info.objectId
+		end
+	end
+	-- Not starting interaction if:
+	--  1. already talking with another NPC
+	--  2. automoving to NPC
+	if avatar.IsTalking() or IsNavigateToPoint() then
+		flag = false
+	end
+
+	if flag and userMods.FromWString(currentMap.name) == MapName then
+		for j = 1, GetTableSize(MercOffc) do
+			local name = MercOffc[j]
+			if current[name] and not knownNPCs[name] then
+				avatar.StartInteract(current[name])
+				break
+			end
+		end
+	end
+	knownNPCs = current
+end
+
+
+
+--------------------------------------------------------------------------------
+-- INITIALIZATION
+--------------------------------------------------------------------------------
+function Init()
+	-- События:
+	common.RegisterEventHandler(OnTalkStarted, "EVENT_INTERACTION_STARTED")
+	common.RegisterEventHandler(OnActionFailed, "EVENT_ACTION_FAILED_OTHER")
+	common.RegisterEventHandler(OnContextActionsChanged, "EVENT_CONTEXT_ACTIONS_CHANGED")
+	Init = nil
+end
+--------------------------------------------------------------------------------
+Init()
+--------------------------------------------------------------------------------
---
+++ data/Mods/Addons/CombatsAssist/Locales/localization.lua
@@ -0,0 +1,108 @@
+Global("Locales", {})
+Global("localization", common.GetLocalization())
+--Global("localization", "eng_eu")
+--------------------------------------------------------------------------------
+-- Russian
+--------------------------------------------------------------------------------
+Locales["rus"]={}
+Locales["rus"]["ButtonBuyElixirs"]="Эссенция Грёз"
+Locales["rus"]["ButtonBuyHammers"]="инструменты"
+Locales["rus"]["ButtonBuyGold"]="Золото"
+Locales["rus"]["ButtonBuyNothing"]="Ничего"
+Locales["rus"]["ButtonSuggestionHighestLoses"]="Высший максимум"
+Locales["rus"]["ButtonSuggestionSmallestGain"]="Выше среднего"
+Locales["rus"]["ButtonSuggestionSuggestionOff"]="Нет предложений"
+Locales["rus"]["ButtonUnequipOnFast"]="Снимите быстро"
+Locales["rus"]["ButtonUnequipOnSlow"]="Снимите медленный"
+Locales["rus"]["ButtonUnequipOff"]="не взлетать"
+Locales["rus"]["Win"]="Выиграть!"
+Locales["rus"]["Lose"]="проиграть!"
+Locales["rus"]["UnequipFastLag"]="Мне «могучему ПК» пришлось сделать еще одну неудачную попытку, если она не исчезнет, вы должны позволить мне работать в медленном режиме!"
+Locales["rus"]["HigherMaximumExplain"]="Команда с наибольшим максимальным количеством очков должна проиграть"
+Locales["rus"]["HigherAverageExplain"]="Команда с более высоким средним количеством очков должна проиграть"
+Locales["rus"]["UnequipFastExplain"]="Теперь вы будете быстро экипировать или экипировать свое снаряжение в зависимости от исхода битвы."
+Locales["rus"]["UnequipSlowExplain"]="Теперь вы будете медленно экипировать или экипировать свое снаряжение по совету исхода битвы"
+--------------------------------------------------------------------------------
+-- English
+--------------------------------------------------------------------------------
+Locales["eng_eu"]={}
+Locales["eng_eu"]["ButtonBuyElixirs"]="Dream Elixirs"
+Locales["eng_eu"]["ButtonBuyHammers"]="Excellent Tools"
+Locales["eng_eu"]["ButtonBuyGold"]="Gold"
+Locales["eng_eu"]["ButtonBuyNothing"]="Nothing"
+Locales["eng_eu"]["ButtonSuggestionHighestLoses"]="Higher Maximum"
+Locales["eng_eu"]["ButtonSuggestionSmallestGain"]="Higher Average"
+Locales["eng_eu"]["ButtonSuggestionSuggestionOff"]="Suggestion Off"
+Locales["eng_eu"]["ButtonUnequipOnFast"]="Unequip Fast"
+Locales["eng_eu"]["ButtonUnequipOnSlow"]="Unequip Slow"
+Locales["eng_eu"]["ButtonUnequipOff"]="Unequip Off"
+Locales["eng_eu"]["Win"]="Win!"
+Locales["eng_eu"]["Lose"]="Lose!"
+Locales["eng_eu"]["UnequipFastLag"]="I 'the mighty pc' had to do another unequip try if it persists you should let me work in slow mode!"
+Locales["eng_eu"]["HigherMaximumExplain"]="Team with highest maximum points should lose"
+Locales["eng_eu"]["HigherAverageExplain"]="Team with higher average points should lose"
+Locales["eng_eu"]["UnequipFastExplain"]="Now you will fast unequip or equip your gear on battle outcome suggestion"
+Locales["eng_eu"]["UnequipSlowExplain"]="Now you will slowly unequip or equip your gear on battle outcome suggestion"
+--------------------------------------------------------------------------------
+-- German
+--------------------------------------------------------------------------------
+Locales["ger"]={}
+Locales["ger"]["ButtonBuyElixirs"]="Dream Elixirs"
+Locales["ger"]["ButtonBuyHammers"]="Excellent Tools"
+Locales["ger"]["ButtonBuyGold"]="Gold"
+Locales["ger"]["ButtonBuyNothing"]="Nothing"
+Locales["ger"]["ButtonSuggestionHighestLoses"]="Higher Maximum"
+Locales["ger"]["ButtonSuggestionSmallestGain"]="Higher Average"
+Locales["ger"]["ButtonSuggestionSuggestionOff"]="Suggestion off"
+Locales["ger"]["ButtonUnequipOnFast"]="Unequip Fast"
+Locales["ger"]["ButtonUnequipOnSlow"]="Unequip Slow"
+Locales["ger"]["ButtonUnequipOff"]="Unequip Off"
+Locales["ger"]["Win"]="Win!"
+Locales["ger"]["Lose"]="Lose!"
+Locales["ger"]["UnequipFastLag"]="I 'the mighty pc' had to do another unequip try if it persists you should let me work in slow mode!"
+Locales["ger"]["HigherMaximumExplain"]="Team with highest maximum points should lose"
+Locales["ger"]["HigherAverageExplain"]="Team with higher average points should lose"
+Locales["ger"]["UnequipFastExplain"]="Now you will fast unequip or equip your gear on battle outcome suggestion"
+Locales["ger"]["UnequipSlowExplain"]="Now you will slowly unequip or equip your gear on battle outcome suggestion"
+--------------------------------------------------------------------------------
+-- French
+--------------------------------------------------------------------------------
+Locales["fra"]={}
+Locales["fra"]["ButtonBuyElixirs"]="Dream Elixirs"
+Locales["fra"]["ButtonBuyHammers"]="Excellent Tools"
+Locales["fra"]["ButtonBuyGold"]="Gold"
+Locales["fra"]["ButtonBuyNothing"]="Nothing"
+Locales["fra"]["ButtonSuggestionHighestLoses"]="Higher Maximum"
+Locales["fra"]["ButtonSuggestionSmallestGain"]="Higher Average"
+Locales["fra"]["ButtonSuggestionSuggestionOff"]="Suggestion off"
+Locales["fra"]["ButtonUnequipOnFast"]="Unequip Fast"
+Locales["fra"]["ButtonUnequipOnSlow"]="Unequip Slow"
+Locales["fra"]["ButtonUnequipOff"]="Unequip Off"
+Locales["fra"]["Win"]="Win!"
+Locales["fra"]["Lose"]="Lose!"
+Locales["fra"]["UnequipFastLag"]="I 'the mighty pc' had to do another unequip try if it persists you should let me work in slow mode!"
+Locales["fra"]["HigherMaximumExplain"]="Team with highest maximum points should lose"
+Locales["fra"]["HigherAverageExplain"]="Team with higher average points should lose"
+Locales["fra"]["UnequipFastExplain"]="Now you will fast unequip or equip your gear on battle outcome suggestion"
+Locales["fra"]["UnequipSlowExplain"]="Now you will slowly unequip or equip your gear on battle outcome suggestion"
+--------------------------------------------------------------------------------
+-- Turkish
+--------------------------------------------------------------------------------
+Locales["tr"]={}
+Locales["tr"]["ButtonBuyElixirs"]="Dream Elixirs"
+Locales["tr"]["ButtonBuyHammers"]="Excellent Tools"
+Locales["tr"]["ButtonBuyGold"]="Gold"
+Locales["tr"]["ButtonBuyNothing"]="Nothing"
+Locales["tr"]["ButtonSuggestionHighestLoses"]="Higher Maximum"
+Locales["tr"]["ButtonSuggestionSmallestGain"]="Higher Average"
+Locales["tr"]["ButtonSuggestionSuggestionOff"]="Suggestion off"
+Locales["tr"]["ButtonUnequipOnFast"]="Unequip Fast"
+Locales["tr"]["ButtonUnequipOnSlow"]="Unequip Slow"
+Locales["tr"]["ButtonUnequipOff"]="Unequip Off"
+Locales["tr"]["Win"]="Win!"
+Locales["tr"]["Lose"]="Lose!"
+Locales["tr"]["UnequipFastLag"]="I 'the mighty pc' had to do another unequip try if it persists you should let me work in slow mode!"
+Locales["tr"]["HigherMaximumExplain"]="Team with highest maximum points should lose"
+Locales["tr"]["HigherAverageExplain"]="Team with higher average points should lose"
+Locales["tr"]["UnequipFastExplain"]="Now you will fast unequip or equip your gear on battle outcome suggestion"
+Locales["tr"]["UnequipSlowExplain"]="Now you will slowly unequip or equip your gear on battle outcome suggestion"
