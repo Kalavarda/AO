21393d7cae5e2563f5b379820b3d37db395542a7
kalavarda <kulerda.kalavarda@gmail.com>
2022-02-12 23:20:07 +0300

    1

---
+++ data/Mods/Addons/SundukTarget/script/locales_utf(fra).lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+-- LOCALES
+--------------------------------------------------------------------------------
+Global( "Locales", {
+	["fra﻿"] = { -- French﻿﻿﻿﻿ utf8,
+		["Zones_On"]={["Ruines d'Al Rihat"]=true, ["Éclats d'Irène"]=true,["Suslanger"]=true},
+        ["Cassette"]={["Ruines d'Al Rihat"]="Une Cassette ﻿est proche!"},
+        ["Poussière de fée"]={["Éclats d'Irène"]="Une Poussière de fée est proche!"},
+        ["Écu"]={["﻿Éclats d'Irène"]="Une bourse d'Écu est proche!"},
+        ["Trésor"]={["Suslanger"]="Un trésor est proche!"},
+        ["Grand trésor"]={["Suslanger"]="Un grand trésor est proche!"},
+    },
+}
